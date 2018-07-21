2873f8caf3ef97e72752256315e4ccb08bf66ff4
hocine BENKHADRA <miniben90@hotmail.com>
2018-07-21 11:14:19 +0200

    update version for npm

--- mpu9250.js
+++ mpu9250.js
@@ -3,7 +3,7 @@
  * NodeJs Module : MPU9250
  * @author BENKHADRA Hocine
  * @description Simple reading data for node js and mpu9250
- * @version 0.1.9
+ * @version 0.2.0
  * @dependent i2c, extend, sleep
  * @license MIT
  *
